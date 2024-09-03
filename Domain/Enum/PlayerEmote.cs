04ee7fbe8c37a1fd9fb83d20e8652aafece3b165
well <welldtr@gmail.com>
2024-09-02 21:00:42 -0300

    fix: icones fixos

--- Domain/Enum/PlayerEmote.cs
+++ Domain/Enum/PlayerEmote.cs
@@ -4,17 +4,17 @@ namespace DiscordBot.Domain.Enum
 {
     public static class PlayerEmote
     {
-        public static Emote Clear = Emote.Parse("<:erase:1266536061039804456>");
-        public static Emote Rewind = Emote.Parse("<:rewind:1266536069592256513>");
-        public static Emote Forward = Emote.Parse("<:forward:1266536062277255289>");
-        public static Emote Stop = Emote.Parse("<:stop:1266536076252676197>");
-        public static Emote Play = Emote.Parse("<:play:1266536065032781919>");
-        public static Emote Pause = Emote.Parse("<:pause:1266536063648665772>");
-        public static Emote Skip = Emote.Parse("<:end:1266536059072679948>");
-        public static Emote Repeat = Emote.Parse("<:repeat:1266536136415772756>");
-        public static Emote Shuffle = Emote.Parse("<:shuffle:1266538269924130817>");
-        public static Emote Back = Emote.Parse("<:start:1266536074201661511>");
-        public static Emote Playlist = Emote.Parse("<:playlist:1266536066404454450>");
-        public static Emote Download = Emote.Parse("<:download:1266536056522674258>");
+        public static Emote Clear = Emote.Parse("<:icons8erase64:1280316085341524078>");
+        public static Emote Rewind = Emote.Parse("<:icons8rewind64:1280316241826938932>");
+        public static Emote Forward = Emote.Parse("<:icons8fastforward641:1280316230598787144>");
+        public static Emote Stop = Emote.Parse("<:icons8stop64:1280316071454183434>");
+        public static Emote Play = Emote.Parse("<:icons8play64:1280315949056266270>");
+        public static Emote Pause = Emote.Parse("<:icons8pause64:1280316019184766987>");
+        public static Emote Skip = Emote.Parse("<:icons8end64:1280315995201867806>");
+        public static Emote Repeat = Emote.Parse("<:icons8repeat64:1280316030337421322>");
+        public static Emote Shuffle = Emote.Parse("<:icons8shuffle64:1280316047806828588>");
+        public static Emote Back = Emote.Parse("<:icons8skiptostart64:1280315981855461467>");
+        public static Emote Playlist = Emote.Parse("<:icons8playlist64:1280316005108809800>");
+        public static Emote Download = Emote.Parse("<:icons8downloadingupdates64:1280316058821202043>");
     }
 }
